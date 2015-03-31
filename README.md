0de4c5f7eb51bcda4c6faa3761b5494f1cb1ec65
mgteixeira <mgteixeira@gmail.com>
2015-03-31 16:59:34 +0100

    First Commit  - site template ready to use
    This is the template created for the ideaninja.io. It will evolve over
    time.

---
+++ README.md
@@ -0,0 +1,13 @@
+# ideaNinja.io Website
+
+Release 1.0
+First production Release of ideaninja.io
+Lots of improvements to be made. 
+
+To create a build you need to install a NodeJs with Gulp and the followming modules:
+- Compass
+- Susy
+- Breakpoint
+
+After the modules have been installed open a bash in the repo folder and run gulp, by typing gulp. 
+then copy the contents of the folder "files to add to the build" and you ready to go. 
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    concat = require('gulp-concat');
+    path = require('path');
+
+var env,
+    jsSources,
+    sassSources,
+    htmlSources,
+    outputDir,
+    sassStyle;
+
+env = 'development';
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+jsSources = [
+  'components/scripts/jqloader.js',
+  'components/scripts/TweenMax.min.js',
+  'components/scripts/jquery.scrollmagic.min.js',
+  'components/scripts/script.js'
+];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .on('error', gutil.log)
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload())
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      css: outputDir + 'css',
+      image: outputDir + 'images',
+      style: sassStyle,
+      require: ['susy', 'breakpoint']
+    })
+    .on('error', gutil.log))
+//    .pipe(gulp.dest( outputDir + 'css'))
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);
+  gulp.watch('builds/development/*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload())
+});
+
+// Copy images to production
+gulp.task('move', function() {
+  gulp.src('builds/development/images/**/*.*')
+  .pipe(gulpif(env === 'production', gulp.dest(outputDir+'images')))
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'compass', 'move', 'connect']);
